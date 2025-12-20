1e459eea1af0d0418870d517aa42b1b3c245bcb6
Chamitha Jayavihan Thambiliyagoda <124007079+Chamithjay@users.noreply.github.com>
2025-12-20 11:10:16 +0530

    Added circuit breaker with resilience4j to gateway level

---
+++ apigateway/src/main/java/com/ecommerce/apigateway/FallBackController.java
@@ -0,0 +1,23 @@
+package com.ecommerce.apigateway;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallBackController {
+    @RequestMapping (value="/productService/**", method = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
+    public ResponseEntity<List<String>> productFallBack() {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(Collections.singletonList("Product Service is down"));
+    }
+}
---
+++ apigateway/src/main/java/com/ecommerce/apigateway/GatewayConfig.java
@@ -0,0 +1,43 @@
+package com.ecommerce.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // User Service
+                .route("user", r -> r
+                        .path("/users/**")
+                        .filters(f -> f.rewritePath("/users(?<segment>/?.*)", "/api/users${segment}"))
+                        .uri("lb://USER"))
+
+                // Product Service with Circuit Breaker
+                .route("product", r -> r
+                        .path("/products/**")
+                        .filters(f -> f
+                                .rewritePath("/products(?<segment>/?.*)", "/api/products${segment}")
+                                .circuitBreaker(c -> c
+                                        .setName("ecomBreaker")
+                                        .setFallbackUri("forward:/fallback/productService")))
+                        .uri("lb://PRODUCT"))
+
+                // Order Service
+                .route("order", r -> r
+                        .path("/orders/**")
+                        .filters(f -> f.rewritePath("/orders(?<segment>/?.*)", "/api/orders${segment}"))
+                        .uri("lb://ORDER"))
+
+                // Cart Service
+                .route("cart", r -> r
+                        .path("/cart/**")
+                        .filters(f -> f.rewritePath("/cart(?<segment>/?.*)", "/api/cart${segment}"))
+                        .uri("lb://ORDER"))
+                .build();
+    }
+}
